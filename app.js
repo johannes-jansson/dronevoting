ab573db9b49740a0244fa0a3739f7c3fb5b9ce27
johannes-jansson <johannes.signup@gmail.com>
2017-05-26 20:54:26 +0200

    mostly styling

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("form").style.display="none";
+  //document.getElementById("voted").style.display="none";
+  // document.getElementById("signout").style.display="none";
+  document.getElementById("login").style.display="none";
+
+  // document.getElementById("loginbutton").onclick = function () {
+  //   document.getElementById("form").style.display="unset";
+  //   document.getElementById("login").style.display="none";
+  //   document.getElementById("signout").style.display="unset";
+  // }
+});
