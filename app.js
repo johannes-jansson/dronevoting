e4867878bcb2f1146813751a22a87c59c48eb1c7
johannes-jansson <johannes.signup@gmail.com>
2017-05-27 18:59:37 +0200

    hide class now workign

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
   //   document.getElementById("login").style.display="none";
   //   document.getElementById("signout").style.display="unset";
   // }
+
+  document.getElementById("loginbutton").onclick = function () {
+    document.getElementById("hiddenidentifier").value = document.getElementById("identifier").value;
+    document.getElementById("form").classList.remove('hide');
+    document.getElementById("login").classList.add('hide');
+    document.getElementById("signout").classList.remove('hide');
+    console.log("l");
+  }
 });
